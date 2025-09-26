bccbe377462f5a775d4bc2ecc99cc25f1871984b
makeadifferentworld <gustyiacc@gmail.com>
2025-09-26 16:41:25 -0300

    Update cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -2,7 +2,7 @@
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
-import { analyze } from "../lib/analyzer.js";
+import { analyze } from "../src/analyzer.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
